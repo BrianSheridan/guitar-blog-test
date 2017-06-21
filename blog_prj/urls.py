b4013576fa210cf1c375960acb3de677a6018360
BrianSheridan <briansheridan732@gmail.com>
2017-06-21 12:14:43 +0100

    first commit

---
+++ blog_prj/urls.py
@@ -0,0 +1,32 @@
+"""blog_prj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.views.static import serve
+from .settings import MEDIA_ROOT
+from blog.views import im_home, contact_us
+from accounts import urls as accounts_urls
+
+from blog import urls as blog_urls
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', im_home, name="index"),
+    url(r'^contact$', contact_us),
+    url(r'^blog/', include(blog_urls)),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+    url(r'^user/', include(accounts_urls)),
+]
