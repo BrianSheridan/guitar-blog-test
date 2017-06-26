f39b91855a8c68910cacb0e44cea316727054537
BrianSheridan <briansheridan732@gmail.com>
2017-06-26 10:21:49 +0100

    aws heroku

---
+++ blog_prj/env.py
@@ -0,0 +1,11 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "h4(be*z2a11(!9*@k8&5joodxl#+h$f1=zrr+y*%idoj+i0vz4")
+os.environ.setdefault("DEBUG", "True" )
+os.environ.setdefault("DISQUS_SHORTNAME", "mybootcampblog" )
+
+os.environ.setdefault("DATABASE_URL", "postgres://hbzeppzrlaryea:0ff801f4425150973883782feded1e188ad3a2b2802dbcf825f585dafc82891a@ec2-54-217-212-8.eu-west-1.compute.amazonaws.com:5432/dabj5b8vkg6qbl")
+
+os.environ.setdefault("AWS_STORAGE_BUCKET_NAME","briansheridan")
+os.environ.setdefault("AWS_ACCESS_KEY_ID","AKIAJMWYC3NB24YRLAFA")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY","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")
--- blog_prj/settings.py
+++ blog_prj/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+from .env import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h4(be*z2a11(!9*@k8&5joodxl#+h$f1=zrr+y*%idoj+i0vz4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
@@ -49,6 +51,7 @@ INSTALLED_APPS = [
     'django_forms_bootstrap',
     'django_gravatar',
     'accounts',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -74,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -85,12 +89,14 @@ WSGI_APPLICATION = 'blog_prj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
+
+DATABASES = {'default': dj_database_url.parse("postgres://hbzeppzrlaryea:0ff801f4425150973883782feded1e188ad3a2b2802dbcf825f585dafc82891a@ec2-54-217-212-8.eu-west-1.compute.amazonaws.com:5432/dabj5b8vkg6qbl") }
 
 
 # Password validation
@@ -143,3 +149,20 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
